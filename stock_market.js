171b9a9820001c8792bd78f3cb7ca71197066692
Brandon Felty <brcfelty@gmail.com>
2022-08-02 12:41:42 -0600

    brute force profit check

---
+++ stock_market.js
@@ -0,0 +1,21 @@
+const maxProfit = (prices) => {
+  // brute force strategy
+  let dates = [];
+  for (let i = 0; i < prices.length; i++) {
+    for (let j = 1 + i; j < prices.length; j++) {
+      dates.push([prices[i], prices[j]]);
+    }
+  }
+
+  let profit = 0;
+  for (let i = 0; i < dates.length; i++) {
+    if ((dates[i][1] - dates[i][0]) > profit) {
+      profit = dates[i][1] - dates[i][0]
+    }
+  }
+
+  return profit;
+}
+
+const input = [45, 24, 35, 31, 40, 38, 11];
+console.log(maxProfit(input), ": should return 16");
