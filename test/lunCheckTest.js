0ff02a3a708d4aa5ec1539dc5cc7e61401b62fe4
Brandon Felty <brcfelty@gmail.com>
2022-08-02 12:11:48 -0600

    adds tests for lun functions

--- test/lunCheckTest.js
+++ test/lunCheckTest.js
@@ -1,5 +1,5 @@
 const assert = require('chai').assert;
-const {check, prepInput, getCheckDigit, getLunSum} = require("../lun")
+const {check, prepInput, getCheckDigit, getLunSum, sumDigits} = require("../lun")
 describe('check', function () {
   describe("prepInput", () => {
     it("should return an array of digits", () => {
@@ -11,21 +11,29 @@ describe('check', function () {
       assert.equal(getCheckDigit([3,1,7,8,9,3,7,2,9,9,7]), 3);
     })
   });
+  describe("sumDigits", () => {
+    it("should return the correct number", () => {
+      assert.equal(sumDigits(1), 2);
+    })
+    it("should return the correct number", () => {
+      assert.equal(sumDigits(8), 7);
+    })
+  });
   describe("getLunSum", () => {
     it("should return the correct sum", () => {
       assert.equal(getLunSum([1,7,8,9,3,7,2,9,9,7]), 67);
     })
   });
-  xit('should return a valid check digit', function () {
+  it('should return a valid check digit', function () {
     assert.equal(check(79927398713), true);
   });
-  xit('should return a valid check digit', function () {
+  it('should return a valid check digit', function () {
     assert.equal(check(502091324785), true);
   });
-  xit('should return a valid check digit', function () {
-    assert.equal(check(4462009138008), true);
+  it('should return not valid', function () {
+    assert.equal(check(4462009138008), false);
   });
-  xit('should return not valid', function () {
+  it('should return not valid', function () {
     assert.equal(check(1321213621654), false);
   });
 });
