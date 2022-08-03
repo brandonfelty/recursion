01107bdcd29d439de5c483f65ef10899a68f2155
Brandon Felty <brcfelty@gmail.com>
2022-08-03 08:14:52 -0600

    init commit

---
+++ poppin_bottles.js
@@ -0,0 +1,37 @@
+// for every 2 empty bottles, you get one free full bottle of pop
+// for every four bottle caps, you get one free full bottle of pop
+// each bottle is $2 to purchase
+
+// given investment x, how many bottles of pop? 
+
+const redeem = (investment) => {
+  let totalBottles = 0;
+  let fullBottles = 0;
+  let freeBottles = 0;
+  let bottleCaps = 0;
+
+  bottlesPurchased = Math.floor(investment / 2);
+  fullBottles = bottlesPurchased;
+  totalBottles = bottlesPurchased;
+  
+  while (fullBottles >= 2) {
+    freeBottles = Math.floor(fullBottles / 2);
+    totalBottles += freeBottles;
+    fullBottles = totalBottles - (2 * freeBottles);
+    bottleCaps += freeBottles * 2;
+    if (bottleCaps >= 4) {
+      freeBottles += Math.floor(bottleCaps / 4);
+      totalBottles += freeBottles;
+      bottleCaps -= freeBottles * 4;
+      fullBottles += freeBottles;
+    }
+  }
+  return totalBottles;
+};
+
+
+// task 1
+console.log(redeem(10), ": 15 Total Bottles");
+console.log(redeem(20), ": 35 Total Bottles");
+console.log(redeem(30), ": 55 Total Bottles");
+console.log(redeem(40), ": 75 Total Bottles");
