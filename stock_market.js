c04bc306c53e3a7249a53026b3507a602cb67f95
Brandon Felty <brcfelty@gmail.com>
2022-08-03 07:34:04 -0600

    partially completed recursive form

--- stock_market.js
+++ stock_market.js
@@ -23,38 +23,44 @@ const maxProfit = (prices) => {
   // }
 
   // base case
-  // if (prices.length <= 1) {
-  //   return 0;
-  // }
+  if (prices.length <= 1) {
+    return 0;
+  }
 
-  // const middleI = Math.ceil(prices.length / 2);
-  // const left = prices.splice(0, middleI);
-  // const right = prices.splice(-middleI);
-  // console.log(left, right)
+  const recursion = (arr, lhs, rhs) => {
+    if (lhs === rhs) {
+      return (0, arr[lhs], arr[rhs])
+    }
+    const mid = lhs + (rhs, lhs)
+  }
 
-  // const leftBest = maxProfit(left);
-  // const rightBest = maxProfit(right);
+  const middleI = Math.ceil(prices.length / 2);
+  const left = prices.splice(0, middleI);
+  const right = prices.splice(-middleI);
+  console.log(left, right)
+
+  const leftBest = maxProfit(left);
+  const rightBest = maxProfit(right);
   
-  // const crossBest = Math.max(right) - Math.min(left);
-  // console.log(leftBest, rightBest, crossBest)
-  // return Math.max(leftBest, rightBest, crossBest)
+  const crossBest = Math.max(right) - Math.min(left);
+  console.log(leftBest, rightBest, crossBest)
+  return Math.max(leftBest, rightBest, crossBest)
   
-  // return profit;
-
+  return profit;
 
 
 
   // elegant solution
-  let profit = 0;
-  let minimum = prices[0];
+  // let profit = 0;
+  // let minimum = prices[0];
 
-  for (let i = 0; i < prices.length; i++) {
-    minimum = Math.min(minimum, prices[i]);
-    profit = Math.max(profit, prices[i] - minimum);
-    console.log(minimum, profit)
-  }
+  // for (let i = 0; i < prices.length; i++) {
+  //   minimum = Math.min(minimum, prices[i]);
+  //   profit = Math.max(profit, prices[i] - minimum);
+  //   console.log(minimum, profit)
+  // }
 
-  return profit;
+  // return profit;
 }
 
 const input = [45, 24, 35, 31, 40, 38, 11];
